2beb5e808fd7596734741c93d492ded67f350823
RodrigoJManzo <rodrigoj.manzo@gmail.com>
2022-05-26 23:56:41 -0300

    al fin salio

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 
-//variables
-
-let tituloCuento = document.querySelector("#tituloCuento");
-let ending = document.querySelector("#ending");
 
 // constantes
 const boton = document.querySelector("#creador")
@@ -36,25 +32,34 @@ function crear() {
         })
 
 
-    fetch("https://swapi.dev/api/people/")
+    fetch("https://randomuser.me/api/?inc=name")
         .then((resp)=> resp.json())
         .then((data)=>{
-            const nombrePersonaje = document.querySelector("#nombrePersonaje");
-            let nombre = document.createElement('p');
-            nombre.innerText = data[randomNumber(10)];
-            nombrePersonaje.append(nombre);
+            let nombrePersonaje = document.querySelector("#nombrePersonaje");
+            if (nombrePersonaje.innerHTML = ""){
+                nombrePersonaje.removeChild()
+            }else{
+                let nombre = document.createElement('p');
+                let persona = data.results[0]
+                let names = persona.name
+                let primer = names.first
+                let segundo = names.last
+                nombre.innerHTML = primer
+                console.log(nombre)
+                nombrePersonaje.append(nombre)
+            } 
         })
     
 
     let body = document.querySelectorAll(".selector").id;
     
-    if (body = "triste"){
+    if (body == "triste"){
         fetch('data/sad.json')
             .then((res)=> res.json())
             .then((data)=>{
             document.querySelector("#body").textContent = data[randomNumber(6)]
         })
-    } else if (body = "funny"){
+    } else if (body == "funny"){
         fetch('data/funny.json')
             .then((res)=> res.json())
             .then((data)=>{
